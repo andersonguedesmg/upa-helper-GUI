e0aeb232df4ffc4d8f34c52db18b3c7b6e017f0a
Anderson Guedes <anderson.guedes@outlook.com.br>
2021-11-30 08:53:37 -0300

    Tela de consulta médica com dados mocados

--- src/app/modules/medical-care/pages/appointment/appointment/appointment.component.ts
+++ src/app/modules/medical-care/pages/appointment/appointment/appointment.component.ts
@@ -1,15 +1,94 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
+export interface UserData {
+  id: string;
+  name: string;
+  age: string;
+  arrival: string;
+  status: string;
+}
+
+const STATUS: string[] = [
+  'Consulta ',
+  'Triagem',
+  'Observação',
+  'Tratamento',
+  'Espera',
+];
+
+const NAMES: string[] = [
+  'Maia',
+  'Asher',
+  'Olivia',
+  'Atticus',
+  'Amelia',
+  'Jack',
+  'Charlotte',
+  'Theodore',
+  'Isla',
+  'Oliver',
+  'Isabella',
+  'Jasper',
+  'Cora',
+  'Levi',
+  'Violet',
+  'Arthur',
+  'Mia',
+  'Thomas',
+  'Elizabeth',
+];
 @Component({
   selector: 'app-appointment',
   templateUrl: './appointment.component.html',
-  styleUrls: ['./appointment.component.scss']
+  styleUrls: ['./appointment.component.scss'],
 })
-export class AppointmentComponent implements OnInit {
+export class AppointmentComponent implements AfterViewInit {
+  displayedColumns: string[] = ['id', 'name', 'age', 'arrival', 'status'];
+  dataSource: MatTableDataSource<UserData>;
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
-  ngOnInit(): void {
+  constructor() {
+    // Create 100 users
+    const users = Array.from({ length: 100 }, (_, k) => createNewUser(k + 1));
+
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(users);
   }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
+
+function createNewUser(id: number): UserData {
+  const name =
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
+    ' ' +
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))];
+
+  const value = Math.round(Math.random() * 100).toString();
+
+  return {
+    id: id.toString(),
+    name: name,
+    age: value + ' anos',
+    arrival:
+      value + '/' + value + '/' + value + ' - ' + value + 'h' + value + 'm',
+    status: STATUS[Math.round(Math.random() * (STATUS.length - 1))],
+  };
 }
