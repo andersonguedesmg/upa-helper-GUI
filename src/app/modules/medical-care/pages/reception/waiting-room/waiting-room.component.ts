ff650979db23b7160d50ca018e46985e88208b64
Anderson Guedes <anderson.guedes@outlook.com.br>
2021-12-09 10:38:43 -0300

    Sala de Espera mostrando dados da API

--- src/app/modules/medical-care/pages/reception/waiting-room/waiting-room.component.ts
+++ src/app/modules/medical-care/pages/reception/waiting-room/waiting-room.component.ts
@@ -1,64 +1,45 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-
-export interface UserData {
-  id: string;
-  name: string;
-  age: string;
-  arrival: string;
-  status: string;
-}
-
-const STATUS: string[] = [
-  'Consulta ',
-  'Triagem',
-  'Observação',
-  'Tratamento',
-  'Espera',
-];
-
-const NAMES: string[] = [
-  'Maia',
-  'Asher',
-  'Olivia',
-  'Atticus',
-  'Amelia',
-  'Jack',
-  'Charlotte',
-  'Theodore',
-  'Isla',
-  'Oliver',
-  'Isabella',
-  'Jasper',
-  'Cora',
-  'Levi',
-  'Violet',
-  'Arthur',
-  'Mia',
-  'Thomas',
-  'Elizabeth',
-];
+import Attendance from '../../../models/attendance.model';
+import { AttendanceService } from '../../../services/attendance.service';
+import { TriageService } from '../../../services/triage.service';
 
 @Component({
   selector: 'app-waiting-room',
   templateUrl: './waiting-room.component.html',
   styleUrls: ['./waiting-room.component.scss'],
 })
-export class WaitingRoomComponent implements AfterViewInit {
-  displayedColumns: string[] = ['name', 'age', 'arrival', 'status'];
-  dataSource: MatTableDataSource<UserData>;
+export class WaitingRoomComponent implements OnInit, AfterViewInit {
+  displayedColumns: string[] = [
+    'id',
+    'patientName',
+    'patientAge',
+    'date',
+    'attendanceStatus',
+  ];
+  attendanceList: Attendance[] = [];
+  dataSource = new MatTableDataSource<Attendance>(this.attendanceList);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor() {
-    // Create 100 users
-    const users = Array.from({ length: 100 }, (_, k) => createNewUser(k + 1));
+  constructor(
+    public triageService: TriageService,
+    public attendanceService: AttendanceService
+  ) {}
 
-    // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
+  ngOnInit(): void {
+    this.getAttendancesOpenedForTable();
+  }
+
+  getAttendancesOpenedForTable() {
+    this.attendanceService
+      .getAttendancesOpenedForTable()
+      .subscribe((response) => {
+        this.dataSource.data = response as Attendance[];
+      });
   }
 
   ngAfterViewInit() {
@@ -74,22 +55,4 @@ export class WaitingRoomComponent implements AfterViewInit {
       this.dataSource.paginator.firstPage();
     }
   }
-}
-
-function createNewUser(id: number): UserData {
-  const name =
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
-    ' ' +
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))];
-
-  const value = Math.round(Math.random() * 100).toString();
-
-  return {
-    id: id.toString(),
-    name: name,
-    age: value + ' anos',
-    arrival:
-      value + '/' + value + '/' + value + ' - ' + value + 'h' + value + 'm',
-    status: STATUS[Math.round(Math.random() * (STATUS.length - 1))],
-  };
 }
