b5434da32ef7147d670c8432644cf11fb8b27702
Anderson Guedes <anderson.guedes@outlook.com.br>
2021-12-11 12:03:03 -0300

    Inicio da Tela de Consulta

--- src/app/modules/medical-care/pages/appointment/appointment-register/appointment-register.component.ts
+++ src/app/modules/medical-care/pages/appointment/appointment-register/appointment-register.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import Attendance from '../../../models/attendance.model';
+import { AttendanceService } from '../../../services/attendance.service';
+import { TriageService } from '../../../services/triage.service';
 
 @Component({
   selector: 'app-appointment-register',
   templateUrl: './appointment-register.component.html',
-  styleUrls: ['./appointment-register.component.scss']
+  styleUrls: ['./appointment-register.component.scss'],
 })
 export class AppointmentRegisterComponent implements OnInit {
+  attendanceId!: number;
+  attendanceData!: Attendance;
+  triageDate!: Date;
+  selectedPainIntensity: number = 4;
+  userIdLogged: string = '';
 
-  constructor() { }
+  constructor(
+    public triageService: TriageService,
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    public attendanceService: AttendanceService,
+    private router: Router,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.attendanceId = params['id'];
+    });
+    this.triageDate = new Date();
+    this.attendanceService
+      .getAttendanceById(this.attendanceId)
+      .subscribe((response) => {
+        this.attendanceData = response;
+      });
   }
-
 }
