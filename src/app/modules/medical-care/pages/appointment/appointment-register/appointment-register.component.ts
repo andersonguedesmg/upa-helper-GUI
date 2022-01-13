1b98ce7f81dbb2cbaddf00c72426dad39709de2e
Anderson Guedes <anderson.guedes@outlook.com.br>
2022-01-13 14:44:06 -0300

    Salvar essa Consulta

--- src/app/modules/medical-care/pages/appointment/appointment-register/appointment-register.component.ts
+++ src/app/modules/medical-care/pages/appointment/appointment-register/appointment-register.component.ts
@@ -2,9 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
+import {
+  ConfirmDialogModel,
+  ConfirmDialogComponent,
+} from 'src/app/shared/components/confirm-dialog/confirm-dialog.component';
+import { CommonHelper } from 'src/app/shared/helpers/common.helper';
 import Attendance from '../../../models/attendance.model';
+import { AppointmentService } from '../../../services/appointment.service';
 import { AttendanceService } from '../../../services/attendance.service';
-import { TriageService } from '../../../services/triage.service';
 
 @Component({
   selector: 'app-appointment-register',
@@ -14,9 +19,7 @@ import { TriageService } from '../../../services/triage.service';
 export class AppointmentRegisterComponent implements OnInit {
   attendanceId!: number;
   attendanceData!: Attendance;
-  triageDate!: Date;
-  selectedPainIntensity: number = 4;
-  userIdLogged: string = '';
+  appointmentDate!: Date;
   public form: FormGroup;
   public diagnosis = this.fb.control('', {
     validators: [Validators.maxLength(255)],
@@ -36,7 +39,7 @@ export class AppointmentRegisterComponent implements OnInit {
   });
 
   constructor(
-    public triageService: TriageService,
+    public appointmentsService: AppointmentService,
     private fb: FormBuilder,
     private route: ActivatedRoute,
     public attendanceService: AttendanceService,
@@ -55,12 +58,44 @@ export class AppointmentRegisterComponent implements OnInit {
     this.route.params.subscribe((params) => {
       this.attendanceId = params['id'];
     });
-    this.triageDate = new Date();
+    this.appointmentDate = new Date();
     this.attendanceService
       .getAttendanceById(this.attendanceId)
       .subscribe((response) => {
         this.attendanceData = response;
         console.log(response);
       });
+  }
+
+  createAppointment() {
+    if (this.form.valid) {
+      const title = `Salvar essa Consulta`;
+      const message = `Você tem certeza de que quer salvar essa consulta?`;
+      const dialogData = new ConfirmDialogModel(title, message);
+      const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+        maxWidth: '400px',
+        data: dialogData,
+      });
+      let userIdLogged = CommonHelper.getUserIdLogged();
+      this.form = this.fb.group({
+        ...this.form.value,
+        attendanceId: Number(this.attendanceId),
+        appointmentDate: this.appointmentDate,
+        userId: Number(userIdLogged),
+      });
+      dialogRef.afterClosed().subscribe((dialogResult) => {
+        if (dialogResult == true) {
+          var request = this.form.value;
+          this.appointmentsService.createAppointment(request).subscribe(() => {
+            this.clearForm();
+            this.router.navigate(['/triagem']);
+          });
+        }
+      });
+    }
+  }
+
+  clearForm() {
+    this.form.reset();
   }
 }
--- src/app/modules/medical-care/services/appointment.service.ts
+++ src/app/modules/medical-care/services/appointment.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, take } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import Appointment from '../models/appointment.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AppointmentService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  protected handleError(error: any): Observable<any> {
+    console.error('Erro =>', error);
+    return throwError(error.error.message);
+  }
+
+  public createAppointment(appointment: Appointment): Observable<Appointment> {
+    return this.http
+      .post(environment.baseApiUrl + 'appointments', appointment)
+      .pipe(catchError(this.handleError), take(1));
+  }
 }
