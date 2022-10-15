9595aa13a48046ea8521083771b1e06dab49e153
Anderson Guedes <anderson.guedes@outlook.com.br>
2022-10-15 10:00:42 -0300

    Mensagem de Saudação no Login | Retirada de Warnings do Console

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -2,12 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import jwt_decode from 'jwt-decode';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
+import { CommonHelper } from 'src/app/shared/helpers/common.helper';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) {}
+
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
 
   async login(user: any) {
     const result = await this.http
@@ -18,6 +27,7 @@ export class AuthService {
       let decoded: any = jwt_decode(result.token);
       window.localStorage.setItem('userNameLogged', decoded.name);
       window.localStorage.setItem('userIdLogged', decoded.id);
+      this.greetingMessage();
       return true;
     }
 
@@ -58,5 +68,28 @@ export class AuthService {
     }
 
     return true;
+  }
+
+  greetingMessage() {
+    let greetingMessage = '';
+    let userNameLogged = CommonHelper.getUserNameLogged();
+    let hour = new Date().getHours();
+
+    if (hour <= 5) {
+      greetingMessage = `Boa madrugada, ${userNameLogged}!`;
+    } else if (hour < 12) {
+      greetingMessage = `Bom dia, ${userNameLogged}!`;
+    } else if (hour < 18) {
+      greetingMessage = `Boa tarde, ${userNameLogged}!`;
+    } else {
+      greetingMessage = `Boa noite, ${userNameLogged}!`;
+    }
+
+    this.snackBar.open(greetingMessage, 'X', {
+      duration: 4000,
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      panelClass: ['success-snackbar'],
+    });
   }
 }
--- src/app/modules/medical-care/pages/triage/triage-register/triage-register.component.ts
+++ src/app/modules/medical-care/pages/triage/triage-register/triage-register.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import {
   MatSnackBar,
